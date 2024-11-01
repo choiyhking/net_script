5d91e064ba479aead72f20bbe97db99a28febea8
Yunha Choi <161846673+choiyhking@users.noreply.github.com>
2024-11-01 11:16:13 +0900

    Update do_throughput.sh

--- do_throughput.sh
+++ do_throughput.sh
@@ -15,19 +15,19 @@ mkdir -p ${RESULT_DIR} 2>/dev/null
 
 case "${2}" in
 	_parallel*)
-        rm *parallel* 2>/dev/null
+        rm *${2}* 2>/dev/null
 		NEW_RESULT_FILE=${RESULT_FILE}${2}
 		echo "${HEADER}" > ${NEW_RESULT_FILE}
 		netperf -H ${SERVER_IP} -l 10 | tail -n 1 >> ${NEW_RESULT_FILE}
 		;;
 	_cpu*)
-        rm *cpu* 2>/dev/null
+        rm *${2}* 2>/dev/null
 		;;&
 	_mem*)
-        rm *mem* 2>/dev/null
+        #rm *${2}* 2>/dev/null
 		;;&
 	_default*)
-		rm *default* 2>/dev/null
+		rm *${2}* 2>/dev/null
 		;;&
 	*)
 		for M_SIZE in 64 128 256 512 1024 2048 4096
