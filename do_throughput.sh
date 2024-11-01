a6fa0536726d36aea8ff2cb1964329adf4a8c788
Yunha Choi <choiyhking@knu.ac.kr>
2024-11-01 11:04:31 +0900

    modify

--- do_throughput.sh
+++ do_throughput.sh
@@ -7,18 +7,18 @@
 SERVER_IP="192.168.51.232"
 RESULT_DIR="$HOME/net_result/${1}/throughput/"
 RESULT_FILE_PREFIX="res_throughput"
-RESULT_FILE="${RESULT_DIR}${RESULT_FILE_PREFIX}${2}"
+RESULT_FILE="${RESULT_DIR}${RESULT_FILE_PREFIX}"
 REPEAT=3
+HEADER="Recv Socket Size(B)  Send Socket Size(B)  Send Message Size(B)  Elapsed Time(s)  Throughput(10^6bps)"
 
 mkdir -p ${RESULT_DIR} 2>/dev/null
-
-echo "Recv Socket Size(B)  Send Socket Size(B)  Send Message Size(B)  Elapsed Time(s)  Throughput(10^6bps)" \
-	> ${RESULT_FILE}
 
 case "${2}" in
 	_parallel*)
         rm *parallel* 2>/dev/null
-		netperf -H ${SERVER_IP} -l 10 | tail -n 1 >> ${RESULT_FILE}
+		NEW_RESULT_FILE=${RESULT_FILE}${2}
+		echo "${HEADER}" > ${NEW_RESULT_FILE}
+		netperf -H ${SERVER_IP} -l 10 | tail -n 1 >> ${NEW_RESULT_FILE}
 		;;
 	_cpu*)
         rm *cpu* 2>/dev/null
@@ -32,7 +32,8 @@ case "${2}" in
 	*)
 		for M_SIZE in 64 128 256 512 1024 2048 4096
 		do
-			mv ${RESULT_FILE} ${RESULT_FILE}${M_SIZE}
+			NEW_RESULT_FILE=${RESULT_FILE}${M_SIZE}
+		    echo "${HEADER}" > ${NEW_RESULT_FILE}
 			for i in $(seq 1 ${REPEAT})
 			do
 				netperf -H ${SERVER_IP} -l 10 -- -m ${M_SIZE} | tail -n 1 >> ${RESULT_FILE}
@@ -41,5 +42,5 @@ case "${2}" in
 		;;
 esac
 
-mv ${RESULT_FILE} ${RESULT_FILE}.txt
+mv ${NEW_RESULT_FILE} ${NEW_RESULT_FILE}.txt
 
