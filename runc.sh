a8faf3dfdaa0fc4cae770d3fd1ec6152b97cc55e
Yunha Choi <161846673+choiyhking@users.noreply.github.com>
2024-11-04 21:48:11 +0900

    Update runc.sh

--- runc.sh
+++ runc.sh
@@ -117,10 +117,10 @@ else
 	for M_SIZE in ${M_SIZES[@]}
 	do
 	    	RESULT_FILE=${RESULT_FILE_PREFIX}_default_${M_SIZE}
+      		echo "$(pgrep [n]etperf)"
 		sudo sh -c "sleep 5; pidstat -p $(pgrep [n]etperf) 1 10 > ${RESULT_FILE}_CPU" &
 		PIDSTAT_PID=$!
   		RESULT_FILE="${RESULT_FILE/$HOME/\/root}"
-    		echo "${RESULT_FILE}"
 		sudo docker exec ${CONTAINER_NAME} /root/net_script/do_throughput.sh ${RESULT_FILE} ${REPEAT}
 		kill ${PIDSTAT_PID}
 	done
