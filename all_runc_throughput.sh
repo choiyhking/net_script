17fef98f229c86e10919ee408fdd12b4ec7bd5f8
Yunha Choi <choiyhking@knu.ac.kr>
2024-11-08 09:03:17 +0900

    update

--- all_runc_throughput.sh
+++ all_runc_throughput.sh
@@ -4,42 +4,44 @@
 REPEAT=10
 
 echo "****BACKUP AND REMOVE EXISTING RESULTS****"
-sudo cp -r net_result net_result.bak
+sudo rm -rf net_result.bak
+sudo cp -r net_result net_result.bak 2> /dev/null
 sudo rm -rf net_result
-
 
 echo "****START <DEFAULT> EXPERIMENT****"
 # default
-./runc_throughput.sh -r ${REPEAT}
+./runc_throughput.sh -r "${REPEAT}" 
 
 echo "****START <CPU> EXPERIMENT****"
 # CPU 
 for arg in 1 2 4
 do
-	./runc_throughput.sh -r ${REPEAT} -c ${arg}
+	./runc_throughput.sh -r "${REPEAT}" -c "${arg}"
 done
 
 echo "****START <MEMORY> EXPERIMENT****"
 # Memory
 for arg in 512m 1G 2G 3G
 do
-	./runc_throughput.sh -r ${REPEAT} -m ${arg}
+	./runc_throughput.sh -r "${REPEAT}" -m "${arg}"
 done
+
 
 echo "****START <STREAM> EXPERIMENT****"
 # Stream
 for arg in 1 5 10
 do
-	./runc_throughput.sh -r ${REPEAT} -s ${arg}
+	./runc_throughput.sh -r "${REPEAT}" -s "${arg}"
 done
 
 echo "****START <CONCURRENCY> EXPERIMENT****"
 # Concurrency
 for arg in 1 4 8
 do
-	./runc_throughput.sh -r ${REPEAT} -n ${arg}
+	./runc_throughput.sh -r "${REPEAT}" -n "${arg}"
 done
 
-echo "****************************************************"
-echo "****ALL EXPERIMENTS ARE SUCCESSFULLY FINISHED !!****"
-echo "****************************************************"
+
+echo "***************************************************"
+echo "**** ALL EXPERIMENTS ARE SUCCESSFULLY FINISHED ****"
+echo "***************************************************"
